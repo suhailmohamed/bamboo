03b8ebb1706b87ed25e8af3f20377c21131cf140
suhailmohamed <suhailmohamed333@gmail.com>
2020-04-10 14:40:00 +0530

    Add build folder
    Add build folder

---
+++ build/precache-manifest.fc3a9333af52c17e1c91d86cd95386c2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6ba47e2e9ae88e3e09e1",
+    "url": "/bamboo/static/css/main.6f4d4ec0.chunk.css"
+  },
+  {
+    "revision": "6ba47e2e9ae88e3e09e1",
+    "url": "/bamboo/static/js/main.6ba47e2e.chunk.js"
+  },
+  {
+    "revision": "b3ffccb6a756a51140cf",
+    "url": "/bamboo/static/js/runtime~main.b3ffccb6.js"
+  },
+  {
+    "revision": "65e1b10843e1b9dd5bae",
+    "url": "/bamboo/static/js/2.65e1b108.chunk.js"
+  },
+  {
+    "revision": "3c23c650b171b35218619eff6942817d",
+    "url": "/bamboo/index.html"
+  }
+];
